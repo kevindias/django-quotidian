491df241c7ff3e2ab732a3bce232f484603feb68
Kevin Dias <kevin@kevindias.com>
2013-07-23 15:40:41 -0700

    Added template inclusion tag which fetches quotes.
    This means that if Quotidian is only used on a small number of pages
    we can avoid using the context processor for every request.
    
    Needs documentation.

---
+++ quotidian/templatetags/quotidian_tags.py
@@ -0,0 +1,11 @@
+from django import template
+
+from quotidian.models import Quote
+
+
+register = template.Library()
+
+@register.inclusion_tag('quotidian_quote.html')
+def random_quote():
+    quote = Quote.objects.get_random()
+    return {'random_quote': quote}
